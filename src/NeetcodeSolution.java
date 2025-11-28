ce44804999db8fe6e8ac15c02d124fe9983cad4e
DuongTT <trinhtungduong5b1@gmail.com>
2025-11-28 19:34:39 +0700

    Practice: 15

--- src/NeetcodeSolution.java
+++ src/NeetcodeSolution.java
@@ -186,33 +186,35 @@ public class NeetcodeSolution {
         }
         return new int[]{-1,-1};
     }
-    // 15. 3sum (DO 1/2)
+    // 15. 3sum
     public List<List<Integer>> threeSum(int[] nums) {
+        int length = nums.length;
         List<List<Integer>> res = new ArrayList<>();
+        if(length < 3) return res;
         Arrays.sort(nums);
         int left = 0;
         int right = 0;
         int sum = 0;
-        for(int i=0;i<nums.length-2;i++){
+        for(int i=0;i<length-2;i++){
             if(nums[i] > 0) break;
             if(i > 0 && nums[i] == nums[i-1]) continue;
             left = i+1;
-            right = nums.length-1;
+            right = length-1;
             while (left < right){
                 sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0) {
+                if(sum == 0){
                     res.add(List.of(nums[i], nums[left], nums[right]));
                     left += 1;
                     right -= 1;
                     while (left < right && nums[left] == nums[left-1])
                         left += 1;
                 }
-                else if(sum < 0) {
+                else if(sum < 0){
                     left += 1;
                     while (left < right && nums[left] == nums[left-1])
                         left += 1;
                 }
-                else {
+                else{
                     right -= 1;
                     while (left < right && nums[right] == nums[right+1])
                         right -= 1;
