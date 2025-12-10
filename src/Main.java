0ac820bea37b30a58d32f0fd81e1e9969503e871
DuongTT <trinhtungduong5b1@gmail.com>
2025-12-10 09:50:12 +0700

    Practice: 621, 235, 3583

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     /** Neetcode:
-     * Heap - 621 (2/3)
-     * Tree - 235 (1/2)
      * Dairy:
-     * 3583 (1/3)
      */
     public static void main(String[] args) {
         var solution = new Solution();
