c26d1e0763460b1703aa8678dea1a09c31cf0efd
DuongTT <trinhtungduong5b1@gmail.com>
2025-12-15 08:52:45 +0700

    Refactor

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     /** Neetcode:
-     * Tree - 105 (2/3)
      * Dairy:
      */
     public static void main(String[] args) {
         var solution = new Solution();
         var dailySolution = new DailySolution();
         var weeklySolution = new WeeklySolution();
-        String input = "PPPPPPPSPPPSPPPPSPPPSPPPPPSPPPSPPSPPSPPPPPSPSPPPPPSPPSPPPPPSPPSPPSPPPSPPPPSPPPPSPPPPPSPSPPPPSPSPPPSPPPPSPPPPPSPSPPSPPPPSPPSPPSPPSPPPSPPSPSPPSSSS";
-        System.out.println(dailySolution.numberOfWays(input));
     }
 }
