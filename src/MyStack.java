7aa89f9fdf17a2cd82f5e84e43376caa230c9c7a
DuongTT <trinhtungduong5b1@gmail.com>
2026-01-03 22:16:09 +0700

    Do: 225

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-public class MyStack {
+/** <i>Stack</i> - 225. Impliment Stack Using Queues */
+class MyStack {
+    Deque<Integer> stack;
+    public MyStack() {
+        stack = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        stack.offer(x);
+    }
+
+    public int pop() {
+        return stack.pollLast();
+    }
+
+    public int top() {
+        return stack.peekLast();
+    }
+
+    public boolean empty() {
+        return stack.isEmpty();
+    }
 }
