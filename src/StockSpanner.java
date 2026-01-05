99119f296e24a64861efbcb989013cdcf8cd6011
DuongTT <trinhtungduong5b1@gmail.com>
2026-01-05 10:12:26 +0700

    Do: 901

--- src/StockSpanner.java
+++ src/StockSpanner.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
-public class StockSpanner {
+/** <i>Stack</i> - 901. Online Stock Span */
+class StockSpanner {
+    Stack<int[]> st;
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+
+    public int next(int price) {
+        int[] nextPrice = new int[]{price, 1};
+        while (!st.isEmpty()){
+            var curPrice = st.peek();
+            if(curPrice[0] <= price){
+                st.pop();
+                nextPrice[1] += curPrice[1];
+            }
+            else break;
+        }
+        st.add(nextPrice);
+        return nextPrice[1];
+    }
 }
